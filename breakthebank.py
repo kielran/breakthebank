5236852c0cbc25ffbd44b717a9331274f3046162
Jane R <jane_ran@foxitsoftware.com>
2023-02-10 23:32:22 -0800

    add game icon, audio, level select, sample stage

--- breakthebank.py
+++ breakthebank.py
@@ -1,18 +1,28 @@
-# Simple pygame program
-
+#--------------------------------------------------------
 # Import and initialize the pygame library
-import pygame
-import os
+#--------------------------------------------------------
+import pygame, os, random
 pygame.init()
+pygame.mixer.init()
 
+#--------------------------------------------------------
+# Check if the images are loaded
+#--------------------------------------------------------
 if not os.path.exists("imgs"):
     print("you do not have imgs folder")
 
-# files:
-
+#--------------------------------------------------------
+# Sets the bar icon and text for game window
+# Defines size for convert()
+#--------------------------------------------------------
 pygame.display.set_caption('Break the Bank')
+gameicon = pygame.image.load("imgs/game_icon.png")
+pygame.display.set_icon(gameicon)
+size = 80
 
-
+#--------------------------------------------------------
+# Define buttons
+#--------------------------------------------------------
 class button():
     def __init__(self, x,y,width,height, text='', color = None,image=None, hovering_image=None, amplifier=None):
         self.color = color
@@ -87,22 +97,48 @@ class button():
                     return True
             
             return False
-size = 80
 
+#--------------------------------------------------------
+# Define music
+#--------------------------------------------------------
+def music():
+    pygame.mixer.music.load("audio/menu_maintheme.mp3")
+    pygame.mixer.music.play()
+
+    #while True:
+music()            
+
+#--------------------------------------------------------
+# Control objects on the Title Screen/Main Menu
+#--------------------------------------------------------
 def drawMainMenu():
     backgroundphoto = pygame.image.load("imgs/start_bare.png")#.convert()
-    #screen.blit(backgroundphoto, (0, 0))
-    #backgroundphoto = pygame.transform.scale(backgroundphoto, (size*16,size*9)) 
     screen.blit(backgroundphoto,(0,0))
     start_button.draw(screen)
     quit_button.draw(screen)
     audio_button.draw(screen)
     accessibility_button.draw(screen)
 
-def drawPauseMenu():
-    backgroundphoto = pygame.image.load("imgs/pause_bg.png").convert()
+#--------------------------------------------------------
+# Control objects on the Stage Selection page
+# - TODO: finalize page, add buttons for stages
+#--------------------------------------------------------
+def drawStageSelection():
+    backgroundphoto = pygame.image.load("imgs/stage_select.png").convert()
     backgroundphoto = pygame.transform.scale(backgroundphoto, (size*16,size*9))
-    screen.blit(backgroundphoto,(0,0)) 
+    screen.blit(backgroundphoto,(0,0))
+    stage_placeholderbutton.draw(screen)
+    quit_mainmenu_button.draw(screen)
+
+#--------------------------------------------------------
+# Control objects on the Pause Menu
+#--------------------------------------------------------
+def drawPauseMenu():
+    backgroundphoto = pygame.image.load("imgs/pause_bg.png")
+    backgroundphoto = pygame.transform.scale(backgroundphoto, (size*16,size*9))
+    backgroundphoto.convert_alpha()
+    #backgroundphoto.set_alpha(10)
+    screen.blit(backgroundphoto, (0,0))
     continue_button.draw(screen)
     restart_button.draw(screen)
     tutorial_button.draw(screen)
@@ -111,50 +147,64 @@ def drawPauseMenu():
     audio_button.draw(screen)
     accessibility_button.draw(screen)
 
-    
-#    green_button.draw(screen,(0,0,0))
-    #quit_button.draw(screen,(0,0,0))
+#--------------------------------------------------------
+# Placeholder for In Game stages
+# - TODO: this might need to be split into a def for 
+#         every individual stage
+#--------------------------------------------------------
+def drawInGame():
+    backgroundphoto = pygame.image.load("imgs/in_game.png")
+    backgroundphoto = pygame.transform.scale(backgroundphoto, (size*16,size*9))
+    screen.blit(backgroundphoto,(0,0))
 
-
+#--------------------------------------------------------
 # Set up the drawing window
+#--------------------------------------------------------
 screen = pygame.display.set_mode([size*16, size*9])
 # screen = pygame.transform.scale(backgroundphoto,[859, 727])
 width = screen.get_width()
 height = screen.get_height()
-
-
-smallfont = pygame.font.SysFont('comicsansms',25)
-text = smallfont.render('quit', True, (255,00,255))
-color_light = (255,255,255)
-color_dark = (0,0,0)
-# Run until the user asks to quit
-
-#green_button = button( 519, 281, 273,53, 'Start Game',(0,255,0))
-#quit_button = button( width/2+50, height/2, 250, 100,"click me",(255, 0, 255))
 
 start_button = button(width/2.807,height/1.6,20,100,'',None,"imgs/buttons/slice_start.png","imgs/buttons/hovering_slice_start.png")
 quit_button = button(width/2.2,height/1.3,20,100,'',None,"imgs/buttons/slice_quit.png", "imgs/buttons/hovering_slice_quit.png")
 quit_mainmenu_button = button(width/2.2,height/1.3,20,100,'',None,"imgs/buttons/slice_quit.png", "imgs/buttons/hovering_slice_quit.png")
 audio_button = button(width/2.015748,height/1.15,20,100,'',None,"imgs/buttons/slice_audio.png", "imgs/buttons/hovering_slice_audio.png")
 accessibility_button = button(width/2.4063063,height/1.15,20,100,'',None,"imgs/buttons/slice_cb.png", "imgs/buttons/hovering_slice_cb.png")
-
-notify_msg = button( width/2+50, height/2, 250, 100,"PRESS ENTER TO GO BACK TO MAIN MENU FOR NOW",(255, 0, 255), None, None)
-
-# xIMG,yIMG= 400, 100
-
 continue_button = button(width/2.4288,height/2.1951219, 20, 100, '', None, "imgs/buttons/slice_continue.png","imgs/buttons/hovering_slice_continue.png",160)
 restart_button = button(width/2.4015,height/1.75182, 20, 100, '', None, "imgs/buttons/slice_restart.png","imgs/buttons/hovering_slice_restart.png",160)
 tutorial_button = button(width/2.424242,height/1.50627, 20, 100, '', None, "imgs/buttons/slice_tutorial.png","imgs/buttons/hovering_slice_tutorial.png",160)
 pause_title = button(width/3.12195,height/11.6129, 20, 100, '', None, "imgs/slice_paused.png","imgs/slice_paused.png",160) #bad code 
 
+# TODO: placeholders for Stage Selection Menu
+stage_placeholderbutton = button(width/18,height/3,0,20,'',None,"imgs/stage_placeholderbutton.png","imgs/stage_placeholderbutton_hover.png")
+notify_msg = button( width/2+50, height/2, 250, 100,"PRESS ENTER TO GO BACK TO MAIN MENU FOR NOW",(255, 0, 255), None, None)
+smallfont = pygame.font.SysFont('comicsansms',25)
+text = smallfont.render('quit', True, (255,00,255))
+color_light = (255,255,255)
+color_dark = (0,0,0)
 
-running = True
-main_menu = True
-pause_menu = False
-in_game = False
+#--------------------------------------------------------
+#Define Booleans for stage states
+#--------------------------------------------------------
+running = True #game run state
+main_menu = True #title screen/main menu
+pause_menu = False #game paused
+stage_selection = False #level/stage select
+in_game = False #in level/stage
+in_cutscene = False #visual story mode
+
+#--------------------------------------------------------
+# Main Game Loop
+# - Clock tick needs to be contained here
+# - Game state is controlled here
+#--------------------------------------------------------
 while running:
+    timer = pygame.time.Clock()
+    timer.tick(60)
+    #-------------MAIN MENU-------------
     if main_menu:
         drawMainMenu()
+        # music()
         # screen.fill((40, 0, 80))
         # Did the user click the window close button?
         for event in pygame.event.get():
@@ -163,9 +213,10 @@ while running:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if start_button.isOver(mouse):
-                    print("start pressed")
+                    print("TRIGGERED start game")
                     main_menu = False
-                    in_game = True
+                    in_game = False
+                    stage_selection = True
                 if quit_button.isOver(mouse):
                     pygame.quit()
             # if event.type == pygame.MOUSEMOTION:
@@ -175,9 +226,30 @@ while running:
                     # print("width: " + str(width/mouse[0]))
                     # print("height: " + str(height/mouse[1]))
                     print(mouse)
+    #-------------STAGE SELECTION-------------
+    elif stage_selection:
+        drawStageSelection()
+        mouse = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if stage_placeholderbutton.isOver(mouse):
+                    print("TRIGGERED stage selection -> in game")
+                    main_menu = False
+                    in_game = True
+                    stage_selection = False
+                if quit_mainmenu_button.isOver(mouse):
+                    main_menu = True
+                    stage_selection = False
+    #-------------IN GAME-------------
     elif not main_menu and not pause_menu and in_game:
         #screen.fill((40, 0, 80))
-        notify_msg.draw(screen)
+        drawInGame()
+        mouse = pygame.mouse.get_pos()
+        main_menu = False
+        in_game = True
+        stage_selection = False
+        in_cutscene = False
+        #notify_msg.draw(screen)
         #print("we are supposed to be in game right now.")
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -185,9 +257,9 @@ while running:
                     pause_menu = True
                 if event.key == pygame.K_RETURN:
                     main_menu = True
-    else:
+    #-------------PAUSE MENU-------------
+    elif pause_menu:
         drawPauseMenu()
-        # pause_title = button(xIMG,yIMG, 20, 100, '', None, "imgs/slice_paused.png","imgs/slice_paused.png",160)
         mouse = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -197,16 +269,16 @@ while running:
                     pause_menu = False
                     in_game = True
                 if quit_button.isOver(mouse):
-                    main_menu = True
+                    main_menu = False
                     pause_menu = False
+                    stage_selection = True
                     in_game = False
-
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE: # go back to game
                     pause_menu = False
                     main_menu = False
                     in_game = True
-            
+    pygame.display.update()
                 #for finding location of button
                 # if event.key == pygame.K_RIGHT:
                 #     xIMG = xIMG+1
@@ -233,7 +305,7 @@ while running:
     # else:
     #     pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40])
     # screen.blit(text , (width/2+50,height/2))
-    pygame.display.update()
+
 
 # Done! Time to quit.
 pygame.quit()
