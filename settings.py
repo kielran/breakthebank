f7d6c3bd9ec991641edce71922c9fa838d417c3d
zhiyuanwen <72575234+zhiyuanwen@users.noreply.github.com>
2023-03-01 05:51:06 -0800

    Updated map to include obstacles and points

---
+++ settings.py
@@ -0,0 +1,18 @@
+level_map = [
+'                            ',
+'                            ',
+'        C                   ',
+' XX    XXX            XX    ',
+' XX P          O            ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+#11 rows
+# tile_size = 64
+# screen_width = 1280
+# screen_height = len(level_map) * tile_size
+
